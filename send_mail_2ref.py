fa83cb136d65f1b3eaac7379df6ac0e8c75d1430
MMorini86 <matteo.morini994@yahoo.com>
2016-01-26 00:52:31 +0000

    Simplified version + attachments + log
    Send one mail at time, investigation of smtp abuse. To bypass the problem the script is launched with a batch script as:
    "FOR /L %G IN (1,1,n) DO python send_mail_2ref.py" where n is the number of remain contact.
    Added attached: CV and cover letter.
    Log of sent mail

--- send_mail_2ref.py
+++ send_mail_2ref.py
@@ -8,6 +8,7 @@ from email.header import Header
 import psycopg2
 import getpass
 import time
+import os
 
 
 def attach(filename, m):
@@ -37,7 +38,7 @@ cur = conn.cursor()
 print("Login to your mailbox to collect application confirmation mail!")
 umail = input("User: ")
 password = getpass.getpass()
-print(password)
+
 # send mail where I'm applied
 query = """SELECT contact_p,jid FROM cwjobs WHERE applied= true
       AND mail_sent = false;"""
@@ -45,55 +46,62 @@ cur.execute(query)
 # mail sent log file
 now = time.strftime("%c")
 now = now.replace(' ', '_').replace('/', '-').replace(':', '-')
-file = open("Log_mail_sent_" + now + ".txt", "w")
-# attachments
-fa = ['160112_Matteo_Morini_CV_en.pdf', '160112_cover_letter_Matteo.pdf']
+file = open("Send_mail_2ref\Log_mail_sent_" + now + ".txt", "w")
+# attachments list
+fa = os.listdir("C:/prova/Attach")
 i = 1
 
 try:
     msg = MIMEMultipart()
+    for z in range(len(fa)):
+            attach('C:/prova/Attach/' + fa[z], msg)
     msg['From'] = umail
+
     s = smtplib.SMTP("smtp.mail.yahoo.com", 587, timeout=10)
     s.set_debuglevel(1)
     s.starttls()
     s.login(umail, password)
 
-    for r in cur.fetchall():
-        name = r[0].split('.')[0]
-        bodym1 = "Dear " + name + ",\n"
-        bodym2 = """This application has been sent with scripts written in
-                 python using several libraries such as BeautifulSoup,
-                 Selenium, Requests and Psycopg2...I would show that I can
-                 manage rather well these kind of stuffs. You can find on
-                 github -->
-                 https://github.com/MMorini86/Automatic-application-cwjobs
-                 the source code of the  scripts that I used to apply for
-                 this position.
-                 Looking forward to hearing from you."""
-        bodym3 = """\nBest regards\nMatteo Morini\nMail: """ + umail + """
-        Mobile: mymobile"""
-        bodym = bodym1 + " ".join(bodym2.split()) + bodym3
-        subj = """README: Note on job application (cwjob reference
-             n.""" + str(r[1]) + """) - Matteo Morini"""
+    r = cur.fetchone()
+    # In case of empty mail field
+    if r[0] is None:
+        quit()
 
-        recipients_emails = r[0]
-        file.write("Mail n. " + str(i) + " sent to: " + recipients_emails +
-                   " ID:" + str(r[1]) + " - " + now + "\r\n")
-        msg['Subject'] = subj
-        msg['To'] = recipients_emails
-        msg.attach(MIMEText(bodym, 'plain'))
+    # msg composition
+    name = r[0].split('.')[0]
+    bodym1 = "Dear " + name + ",\n"
+    bodym2 = """This application has been sent with scripts written in
+             python using several libraries such as BeautifulSoup,
+             Selenium, Requests and Psycopg2...I would show that I can
+             manage rather well these kind of stuffs. You can find on
+             github -->
+             https://github.com/MMorini86/Automatic-application-cwjobs
+             the source code of the  scripts that I used to apply for
+             this position.
+             Looking forward to hearing from you."""
+    bodym3 = """\nBest regards\nMatteo Morini\nMail: """ + umail + """
+    Mobile: 07951489063
+    Linkedin: https://it.linkedin.com/in/matteo-morini-35b03752/en"""
+    bodym = bodym1 + " ".join(bodym2.split()) + bodym3
+    subj = """README: Note on job application (cwjob reference
+           n.""" + str(r[1]) + """) - Matteo Morini"""
 
-        for z in range(len(fa)):
-            attach(fa[z], msg)
+    recipients_emails = r[0]
+    msg['Subject'] = Header(subj, 'utf-8')
+    msg['To'] = recipients_emails
+    msg.attach(MIMEText(bodym, 'plain', 'utf-8'))
 
-        s.sendmail(msg['From'], recipients_emails, msg.as_string())
-        s.quit()
-        query = "UPDATE cwjobs SET mail_sent = true WHERE jid = "+str(r[1])+"""
-                AND contact_p = '""" + r[0] + "';"
-        cur.execute(query)
-        conn.commit()
-        time.sleep(3)
-        i = i + 1
+    s.sendmail(msg['From'], recipients_emails, msg.as_string())
+    # mail log
+    file.write("Mail n. " + str(i) + " sent to: " + recipients_emails +
+               " ID:" + str(r[1]) + " - " + now + "\r\n")
+    # table cwjobs updating 
+    query = "UPDATE cwjobs SET mail_sent = true WHERE jid = "+str(r[1])+"""
+            AND contact_p = '""" + r[0] + "';"
+    cur.execute(query)
+    conn.commit()
+    i = i + 1
 finally:
+    s.quit()
     cur.close()
     conn.close()
